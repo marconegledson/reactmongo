509cb68617cf43d97f60b1035e61f37cc1cdb075
Marcone Almeida <marconegledson@gmail.com>
2020-10-01 10:04:49 -0300

    correct de test

--- src/test/java/com/example/demo/test/TestJava0.java
+++ src/test/java/com/example/demo/test/TestJava0.java
@@ -6,12 +6,23 @@ import java.util.stream.Collectors;
 public class TestJava0 {
 	
 	public static void main(String[] args) {
-		String str = "pqqprq";
+		String str = "pqqqqprq";
 		List<String> chars = str.chars().mapToObj(e -> String.valueOf((char) e)).collect(Collectors.toList());
 		
-		for (int i = 0; i < chars.size(); i++) {
-			if(chars.size() > 2 && chars.get(i).equals(chars.get(i+1))) {
-				chars.remove(i+1);
+		for (int i = 0, j = 0; i < chars.size(); i++) {
+			boolean hasDuplicate = false;
+			j = i+1;
+			while(j < chars.size()) {
+				if(chars.get(i).equals(chars.get(j))) {
+					chars.remove(j);
+					hasDuplicate = true;
+					j = i+1;
+				}else {
+					break;
+				}
+			}
+			
+			if(hasDuplicate) {
 				chars.remove(i);
 				i = -1;
 			}
